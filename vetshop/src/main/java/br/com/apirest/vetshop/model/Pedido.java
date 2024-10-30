807436d34b17c28a0e1dd4e5ed7568fa7fc0caf8
Felipe de Oliveira <deoliveiratech@gmail.com>
2024-10-29 23:22:08 -0400

    atualizacao do trabalho

--- vetshop/src/main/java/br/com/apirest/vetshop/model/Pedido.java
+++ vetshop/src/main/java/br/com/apirest/vetshop/model/Pedido.java
@@ -1,14 +1,109 @@
 package br.com.apirest.vetshop.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Pedido {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String nome;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fornecedor_id")
+    private Fornecedor fornecedor;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "produto_id")
+    private Set<Produto> produto;
+
     private LocalDate dataDeInclusao;
 
     private LocalDate dataDeAlteracao;
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Set<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Set<Produto> produto) {
+        this.produto = produto;
+    }
+
+    public LocalDate getDataDeInclusao() {
+        return dataDeInclusao;
+    }
+
+    public void setDataDeInclusao(LocalDate dataDeInclusao) {
+        this.dataDeInclusao = dataDeInclusao;
+    }
+
+    public LocalDate getDataDeAlteracao() {
+        return dataDeAlteracao;
+    }
+
+    public void setDataDeAlteracao(LocalDate dataDeAlteracao) {
+        this.dataDeAlteracao = dataDeAlteracao;
+    }
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, String nome, Cliente cliente, Fornecedor fornecedor, Set<Produto> produto,
+            LocalDate dataDeInclusao, LocalDate dataDeAlteracao) {
+        this.id = id;
+        this.nome = nome;
+        this.cliente = cliente;
+        this.fornecedor = fornecedor;
+        this.produto = produto;
+        this.dataDeInclusao = dataDeInclusao;
+        this.dataDeAlteracao = dataDeAlteracao;
+    }
 }
--- vetshop/src/main/java/br/com/apirest/vetshop/service/FornecedorServico.java
+++ vetshop/src/main/java/br/com/apirest/vetshop/service/FornecedorServico.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.apirest.vetshop.model.Fornecedor;
 import br.com.apirest.vetshop.repository.IFornecedorRepositorio;
 
+@Service
 public class FornecedorServico implements IBaseServicoJPA<Fornecedor>{
 
     @Autowired
